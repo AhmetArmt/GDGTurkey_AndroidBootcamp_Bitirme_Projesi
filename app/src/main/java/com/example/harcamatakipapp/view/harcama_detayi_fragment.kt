3678ae2a9f1c8a8a258655d5892e4d104cd1c236
AhmetArmt <ahmett-armyt@hotmail.com>
2021-04-25 19:05:58 +0300

    Base Hali Tamamlandi

---
+++ app/src/main/java/com/example/harcamatakipapp/view/harcama_detayi_fragment.kt
@@ -0,0 +1,26 @@
+package com.example.harcamatakipapp.view
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.harcamatakipapp.R
+
+
+class harcama_detayi_fragment : Fragment() {
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_harcama_detayi_fragment, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+}
